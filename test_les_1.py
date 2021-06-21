1d042c2d14e596c513c43407eaeeb06eb4d9d16f
Notker367 <notstrauss@gmail.com>
2021-06-21 22:56:31 +0300

    Задание №2: Создать вспомогательные методы и классы

--- test_les_1.py
+++ test_les_1.py
@@ -27,6 +27,23 @@ def logout_from_homepage(driver):
     driver.find_element_by_link_text("Logout").click()
 
 
+def add_new_group_from_home(driver, group):
+    driver.find_element_by_link_text("groups").click()
+    driver.find_element_by_name("new").click()
+    driver.find_element_by_name("group_name").click()
+    driver.find_element_by_name("group_name").clear()
+    driver.find_element_by_name("group_name").send_keys(group.name)
+    driver.find_element_by_name("group_header").click()
+    driver.find_element_by_name("group_header").clear()
+    driver.find_element_by_name("group_header").send_keys(group.header)
+    driver.find_element_by_name("group_footer").click()
+    driver.find_element_by_name("group_footer").clear()
+    driver.find_element_by_name("group_footer").send_keys(group.footer)
+    driver.find_element_by_name("submit").click()
+    driver.find_element_by_link_text("group page").click()
+    driver.find_element_by_link_text("home").click()
+
+
 class UntitledTestCase(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
@@ -52,6 +69,7 @@ class UntitledTestCase(unittest.TestCase):
         driver: object = self.driver
         open_home_page(driver)
         login(driver, username="admin", password="secret")
+        add_new_group_from_home(driver, Group(name="Name_test", header="Header_test", footer="Footer_test"))
         logout_from_homepage(driver)
 
     def is_element_present(self, how, what):
