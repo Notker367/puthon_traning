e8f0d66d33bbf13c626a02bd2d175d58ce75a4be
Notker367 <notstrauss@gmail.com>
2021-07-01 02:39:16 +0300

    Задание №9: Обеспечить выполнение предусловий в тестах для удаления и модификации для групп и контактов little bit optimized (dont know need it or not)

--- tests/contact/test_del_contact.py
+++ tests/contact/test_del_contact.py
@@ -2,6 +2,5 @@ from model.contact import Contact
 
 
 def test_case_1_delete_first_contact(app):
-    if app.contact.count() == 0:
-        app.contact.create_from_home(Contact(firstname="new_contact_for_del"))
+    app.contact.if_not_to_create(Contact(firstname="new_contact_for_del"))
     app.contact.delete_first()
