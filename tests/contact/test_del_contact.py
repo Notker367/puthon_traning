df03b8b1da161cf87e07d5e62fae375e6803f084
Notker367 <notstrauss@gmail.com>
2021-07-05 22:57:33 +0300

    Задание №11: Реализовать проверки в виде сравнения списков for del

--- tests/contact/test_del_contact.py
+++ tests/contact/test_del_contact.py
@@ -3,4 +3,10 @@ from model.contact import Contact
 
 def test_case_1_delete_first_contact(app):
     app.contact.create_if_not_exist(Contact(firstname="new_contact_for_del"))
+    odl_contacts = app.contact.get_contact_list()
     app.contact.delete_first()
+    new_contacts = app.contact.get_contact_list()
+    assert len(odl_contacts) - 1 == len(new_contacts)
+    odl_contacts[0:1] = []
+    assert odl_contacts == new_contacts
+
