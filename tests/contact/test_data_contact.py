3acb6a5ecd5e91e4359880ac5f7f5d41f1d44374
Notker367 <notstrauss@gmail.com>
2021-07-07 02:09:20 +0300

    Задание №14: Реализовать проверки для всех полей контакта на главной странице hot_fix

--- tests/contact/test_data_contact.py
+++ tests/contact/test_data_contact.py
@@ -5,16 +5,15 @@ import random
 def test_case_1_rdm_contact_data_from_homepage(app):
     app.contact.create_if_not_exist(Contact(firstname="new_contact_for_edit"))
     old_contacts = app.contact.get_contact_list()
-    # index = random.randrange(len(old_contacts))
-    index = 9
+    index = random.randrange(len(old_contacts))
     contact_from_home_page = old_contacts[index]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.all_phones_from_home_page \
            == app.contact.merge_for_contact_it_attributes(
-        attributes=all_phones(contact_from_edit_page))
+                attributes=all_phones(contact_from_edit_page))
     assert contact_from_home_page.all_emails_from_home_page \
            == app.contact.merge_for_contact_it_attributes(
-        attributes=all_email(contact_from_edit_page))
+                attributes=all_email(contact_from_edit_page))
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
     assert contact_from_home_page.address == contact_from_edit_page.address
