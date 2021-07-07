fb02227bed991cc3f9f975c19b8dc00b65af4498
Notker367 <notstrauss@gmail.com>
2021-07-07 03:00:31 +0300

    Задание №15: Параметризовать тесты для создания групп и контактов

--- tests/group/test_add_group.py
+++ tests/group/test_add_group.py
@@ -1,16 +1,34 @@
 from model.group import Group
 import pytest
+import random
+import string
 
-test_data = [
-    Group(name="asdqe", header="asdqeadd", footer="asdfasqe"),
-    Group(name=" ", header=" ", footer=" ")
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
+    return prefix \
+           + "".join(random.choice(symbols)
+                     for i in range(random.randrange(maxlen)))
+
+
+test_data = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("name", 10),
+          header=random_string("header", 20),
+          footer=random_string("footer", 20))
+    for i in range(5)
 ]
 
 
-@pytest.mark.parametrize("group", test_data)
+# test_data = [Group(name="", header="", footer="")]+[
+#    Group(name=name, header=header, footer=footer)
+#    for name in ["", random_string("name", 10)]
+#    for header in ["", random_string("header", 20)]
+#    for footer in ["", random_string("footer", 20)]
+# ]
+
+@pytest.mark.parametrize("group", test_data, ids=[repr(x) for x in test_data])
 def test_case_1_add_new_group(app, group):
     old_groups = app.group.get_group_list()
-    # group = Group(name="asdqe", header="asdqeadd", footer="asdfasqe", )
     app.group.create_from_home(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
@@ -18,13 +36,3 @@ def test_case_1_add_new_group(app, group):
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-@pytest.mark.parametrize("group", test_data)
-def test_case_2_add_empty_group(app, group):
-    pass
-#    old_groups = app.group.get_group_list()
-#    group = Group(name="", header="", footer="", )
-#    app.group.create_from_home(group)
-#    new_groups = app.group.get_group_list()
-#    assert len(old_groups) + 1 == len(new_groups)
-#    old_groups.append(group)
-#    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
