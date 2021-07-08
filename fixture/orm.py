d02d06cc586f3beff661c9aac1f0c7b2b0e94a88
Notker367 <notstrauss@gmail.com>
2021-07-08 18:56:09 +0300

    Задание №19: Реализовать загрузку тестовых данных из файлов  fix

--- fixture/orm.py
+++ fixture/orm.py
@@ -14,6 +14,8 @@ class ORMFixture:
         name = Optional(str, column="group_name")
         header = Optional(str, column="group_header")
         footer = Optional(str, column="group_footer")
+        contacts = Set(lambda: ORMFixture.ORMContact, table="address_in_groups", column="id", reverse="groups",
+                       lazy=True)
 
     class ORMContact(db.Entity):
         _table_ = "addressbook"
@@ -21,6 +23,8 @@ class ORMFixture:
         firstname = Optional(str, column="firstname")
         lastname = Optional(str, column="lastname")
         deprecated = Optional(str, column="deprecated")
+        groups = Set(lambda: ORMFixture.ORMGroup, table="address_in_groups", column="group_id", reverse="contacts",
+                     lazy=True)
 
     def __init__(self, host, name, user, password):
         self.db.bind("mysql", host=host, database=name, user=user, password=password, conv=decoders)
