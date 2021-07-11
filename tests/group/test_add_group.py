7989ec6e48521591b0fbb100bfb12ac86588a755
Notker367 <notstrauss@gmail.com>
2021-07-11 22:01:55 +0300

    Задание №29: Реализовать генерацию отчётов при помощи Allure code

--- tests/group/test_add_group.py
+++ tests/group/test_add_group.py
@@ -1,12 +1,19 @@
 from model.group import Group
+import allure
 
 
 def test_case_1_add_new_group(app, db, json_groups, check_ui):
     group = json_groups
-    old_groups = db.get_group_list()
-    app.group.create_from_home(group)
-    new_groups = db.get_group_list()
-    old_groups.append(group)
-    assert old_groups == new_groups
-    if check_ui:
-        assert sorted(old_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+    with allure.step("Given a group list"):
+        old_groups = db.get_group_list()
+    with allure.step(f"When I add a group {group} to the list"):
+        app.group.create_from_home(group)
+    with allure.step("Then the new group list is equal to the old list with the added group"):
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert old_groups == new_groups
+        if check_ui:
+            assert sorted(old_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+
+
+# D:\python\allure-2.14.0\allure-2.14.0\bin\allure.bat serve allure-results
